8613e087e2e3869755ee9821d06257d7a4c80dfb
Lion Yang <lion@aosc.xyz>
2016-03-22 08:20:18 +0800

    seasun: modify info

--- app.js
+++ app.js
@@ -1,31 +1,7 @@
-// ** DO **
-var express = require('express'); // ← Familiar things
-var bodyParser = require('body-parser');
-// Because it's still the **BACKEND**
-"Neop's love." // you love
-/* only better */
-
-
-// Do blink-of-an-WINK
-var app = express(); // ← things
+var express = require('express');
+var app = express();
 var log = require('./controllers/log.js');
-// With quick start up and performance.
-
-// Do the same things
-// Because
-var routes = require('./controllers/router.js'); // ← your stuff
-// comes with you.
-
-/* Do personal things
-       private
-      whatever        */
-", such as sleeping together,"
-// With a more secure Framework.
-
-// Do
-// to-do things
-'"Plz read the fine manual" says the Jeff'
-app.use(bodyParser.urlencoded({ extended: false }));
+var routes = require('./controllers/router.js');
 var jade = require('jade');
 app.engine('jade', function(path, options, fn) {
     if(options.compileDebug == undefined && process.env.NODE_ENV === 'production') {
@@ -34,53 +10,13 @@ app.engine('jade', function(path, options, fn) {
     options.pretty = true;
     jade.renderFile(path, options, fn);
   });
-app.set('view engine', 'jade'); // Use Lion and Jade
-app.set('views', './views');    // your truly personal digital
-// assistant
-
-// on your home
-// Reminding you to get
-{{};;{};['',"w"];}; // flowers
-
-// and
-// finding that static pages
-app.use(express.static('static')); // on your *public*
-
-// Do unexpected things
-// that turn browsing
-/* into doing */
-
+app.set('view engine', 'jade');
+app.set('views', './views');
+app.use(express.static('static'));
 routes.DoBoom(app);
-// Cha-cha-cha
-
 var child_process = require('child_process');
 child_process.fork('./controllers/watcher.js');
 log.debug('server: Fork watcher');
-// tweet-tweet
-
-// With a one-stop AOSC Web Team
-/*  full of
-    straights,
-    lions,
-    and bis.  */
-
-// Do visual things
 var server = app.listen(3000, function () {
   log.debug('server: Listening at port ' + server.address().port);
 });
-// Like using AOSC OS for
-// Free
-// Zero
-// Zilch
-// Nada
-///////  FREE  ////////
-// Available in 2015 //
-/* Reserve a lion today. */
-
-/*
-
-              AOSC
-          Web Team 2015
-             "Safe"
-
-                              */
